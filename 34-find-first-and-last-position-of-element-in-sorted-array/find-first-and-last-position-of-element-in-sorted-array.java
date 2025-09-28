baf0e978ab5197f685b9c24e2f35ece7f23d53d7
Abhijeet Mishra <158300568+Abhijeet60078@users.noreply.github.com>
2025-09-28 14:08:51 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (49.62%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,44 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] arr = {-1, -1};
-       
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                if (mid == 0 || nums[mid - 1] != target) {
-                    arr[0] = mid;
-                    break;
-                } else {
-                    right = mid - 1;
-                }
-            }
-        }
+        int first=findbound(nums,target,true);
+        int last=findbound(nums,target,false);
+        return new int[]{first,last};
 
         
-        left = 0;
-        right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                if (mid == nums.length - 1 || nums[mid + 1] != target) {
-                    arr[1] = mid;
-                    break;
-                } else {
-                    left = mid + 1;
+
+
+
+
+
+
+
+
+        
+    }
+    private static int findbound(int[] nums,int target,boolean isFirst){
+        int left=0;
+        int right=nums.length-1;
+        int result=-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+
+            if(nums[mid]==target){
+                result=mid;
+                if(isFirst){
+                    right= mid-1;
+                }
+                else{
+                    left=mid+1;
                 }
             }
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
         }
+        return result;
 
-        return arr;
     }
 }
