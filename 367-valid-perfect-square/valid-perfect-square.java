dba913f4b9c088c8e2fbcd164afe0fc08ed3c453
Abhijeet Mishra <158300568+Abhijeet60078@users.noreply.github.com>
2025-09-28 14:26:15 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (19.94%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -3,14 +3,25 @@ class Solution {
         if(num==1){
             return true;
         }
-        
-        for(long i=1;i<num;i++){
-            if(i*i==num){
+        long left=1;
+        long right=num/2;
+        while(left<=right){
+            long mid=(left+right)/2;
+            long sq=mid*mid;
+            if(sq==num){
                 return true;
+
+            }else if(sq<num){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
             }
 
         }
         return false;
+
+        
 
 
         
