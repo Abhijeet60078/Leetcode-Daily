dbb68e14b0532f322d2e21de34d57cfb1022a7ff
Abhijeet Mishra <158300568+Abhijeet60078@users.noreply.github.com>
2025-12-22 17:22:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (20.88%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        int prev=1;
+        int prev1=0;
+        for(int i=2;i<=n;i++){
+            int curr=prev+prev1;
+            prev1=prev;
+            prev=curr;
+        }
+        return prev;
+
+        
+    }
+}
