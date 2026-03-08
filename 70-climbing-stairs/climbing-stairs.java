804815ec842bd5349d47869f9acbed1f31cab57d
Abhijeet Mishra <158300568+Abhijeet60078@users.noreply.github.com>
2026-03-08 22:19:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (99.62%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,15 +1,21 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=2)return n;
-        int a=1;
-        int b=2;
-        for(int i=3;i<=n;i++){
-            int c=a+b;
-            a=b;
-            b=c;
-
-
-        }   
-        return b;     
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        int pin=1;
+        int pin1=1;
+        int min=0;
+        for(int i=2;i<=n;i++){
+            min=pin+pin1;
+            pin1=pin;
+            pin=min;
+        }
+        return min;
+        
+        
     }
 }
