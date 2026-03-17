6be91f247a7d4a427b9bd25efefad2b8780962bc
Abhijeet Mishra <158300568+Abhijeet60078@users.noreply.github.com>
2026-03-17 11:28:42 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (92.19%) - LeetSync

---
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        int p=1;
+
+        for(int i=n;i<=n*2;i++){
+            if(i%2==0&&i%n==0){
+                p=i;
+                break;
+
+                
+
+            }
+        }
+        return p;
+        
+
+    }
+}
